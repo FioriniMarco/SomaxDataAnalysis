5d1d5c08c8ec876fabed248c2d9b2a807fdb973a
Marco Fiorini <marco.fiorini@ircam.fr>
2023-10-23 13:59:45 +0200

    New analyses

--- python/distribution_csv_iterate_full.py
+++ python/distribution_csv_iterate_full.py
@@ -26,21 +26,20 @@ def find_lowest_and_highest_pitches(csv_file_path):
 num_folders = 10
 num_files_per_folder = 18
 
-# Dictionary to store pitch occurrences
-pitch_distribution = {midi_value: 0 for midi_value in range(21, 109)}
-
 # Iterate over folders
 for folder_num in range(1, num_folders+1):
     folder_name = f'impro{folder_num:02d}'  # Format folder name with leading zeros
 
-    # Dictionary to store pitch occurrences
-    pitch_distribution = {midi_value: 0 for midi_value in range(21, 109)}
+    # List to store pitch distributions for each file
+    pitch_distributions = []
 
     # Iterate over files in each folder
     for file_num in range(1, num_files_per_folder+1):
         file_name = f'impro{folder_num:02d}-{file_num:02d}-audioinfluencer.csv'
 
         csv_file_path = os.path.join('data', folder_name, file_name)
+
+        pitch_distribution = {midi_value: 0 for midi_value in range(21, 109)}
 
         lowest_note, highest_note, lowest_midi, highest_midi = find_lowest_and_highest_pitches(csv_file_path)
 
@@ -49,17 +48,34 @@ for folder_num in range(1, num_folders+1):
             if midi_value in pitch_distribution:
                 pitch_distribution[midi_value] += 1
 
-    # Specify the result directory
-    result_directory = os.path.join('results', folder_name)
+        pitch_distributions.append(pitch_distribution)
+
+    # Specify the result directory for pitch distribution
+    result_directory = os.path.join('results', folder_name, 'pitch_distribution')
     os.makedirs(result_directory, exist_ok=True)
+
+    # Transpose the data to have files as rows and MIDI values as columns
+    transposed_data = [[d.get(midi, 0) for d in pitch_distributions] for midi in range(21, 109)]
 
     # Save the pitch distribution to a CSV file
     result_file_path = os.path.join(result_directory, 'pitch_distribution.csv')
     with open(result_file_path, 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['MIDI Value', 'Occurrences'])
-
-        for midi_value, occurrences in pitch_distribution.items():
-            writer.writerow([midi_value, occurrences])
+        writer.writerow(['MIDI Value'] + [f'File {i:02d}' for i in range(1, num_files_per_folder+1)])
+        writer.writerows([[midi] + row for midi, row in zip(range(21, 109), transposed_data)])
 
     print(f'Pitch distribution for folder {folder_name} saved to {result_file_path}.')
+
+    # Calculate global pitch distribution
+    global_pitch_distribution = {midi_value: sum(pitch_distribution.get(midi_value, 0) for pitch_distribution in pitch_distributions) for midi_value in range(21, 109)}
+
+    # Save the global pitch distribution to a CSV file
+    global_result_file_path = os.path.join(result_directory, 'global_pitch_distribution.csv')
+    with open(global_result_file_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['MIDI Value', 'Occurrences'])
+
+        for midi_value, occurrences in global_pitch_distribution.items():
+            writer.writerow([midi_value, occurrences])
+
+    print(f'Global pitch distribution for folder {folder_name} saved to {global_result_file_path}.')
--- python/time_distribution_csv_iterate_full.py
+++ python/time_distribution_csv_iterate_full.py
@@ -22,6 +22,27 @@ def calculate_note_distribution(csv_file_path):
 
     return note_distribution
 
+def calculate_temporal_note_distribution(csv_file_path):
+    with open(csv_file_path, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip header if present
+
+        notes_per_second = {}  # Dictionary to store notes per second
+        for row in reader:
+            time = float(row[0])  # Assuming time is in the first column
+            note = int(row[1])  # Assuming MIDI note is in the second column
+
+            # Calculate the second and update the notes count
+            second = int(time)
+            if second not in notes_per_second:
+                notes_per_second[second] = []
+            notes_per_second[second].append(note)
+
+        # Calculate the number of notes per second
+        notes_distribution = {second: len(notes) for second, notes in notes_per_second.items()}
+
+        return notes_distribution
+
 def save_note_distribution_to_csv(folder_name, file_name, note_distribution):
     result_directory = os.path.join('results', folder_name)
     os.makedirs(result_directory, exist_ok=True)
@@ -33,6 +54,18 @@ def save_note_distribution_to_csv(folder_name, file_name, note_distribution):
         writer.writerows(note_distribution.items())
 
     print(f'Note distribution saved to {result_file_path}.')
+
+def save_temporal_note_distribution_to_csv(folder_name, file_name, temporal_note_distribution):
+    result_directory = os.path.join('results', folder_name, 'temporal_note_distribution')
+    os.makedirs(result_directory, exist_ok=True)
+
+    result_file_path = os.path.join(result_directory, f'{file_name}_temporal_note_distribution.csv')
+    with open(result_file_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Second', 'Note Count'])
+        writer.writerows(temporal_note_distribution.items())
+
+    print(f'Temporal note distribution saved to {result_file_path}.')
 
 # Define the range of folders and files
 num_folders = 10
@@ -51,12 +84,14 @@ for folder_num in range(1, num_folders+1):
         csv_file_path = os.path.join('data', folder_name, file_name)
 
         note_distribution = calculate_note_distribution(csv_file_path)
+        temporal_note_distribution = calculate_temporal_note_distribution(csv_file_path)
 
         print(f'File: {file_name}')
-        for second, note_count in note_distribution.items():
+        for second, note_count in temporal_note_distribution.items():
             print(f'Second {second}: {note_count} notes played')
 
         save_note_distribution_to_csv(folder_name, file_name, note_distribution)
+        save_temporal_note_distribution_to_csv(folder_name, file_name, temporal_note_distribution)
 
         print('\n' + '-'*30 + '\n')
 
